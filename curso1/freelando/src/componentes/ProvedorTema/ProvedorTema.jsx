27517c82a089bb6cb847eb3a8f66b8a2c9ba7d3b
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2025-04-01 12:35:02 -0300

    criando componenetes tipografia e campo de texto

--- curso1/freelando/src/componentes/ProvedorTema/ProvedorTema.jsx
+++ curso1/freelando/src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -16,7 +16,7 @@ const tema = {
             c: ''
         },
         neutras: {
-            a: '',
+            a: '#373737',
             b: '',
             c: ''
         },
@@ -26,6 +26,7 @@ const tema = {
         }
     },
     espacamentos: {
+       xs: '8px',
        s: '16px',
        l: '32px',
     },
